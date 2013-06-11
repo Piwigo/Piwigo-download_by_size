b898cc9b3e33aaeb95089a1662636983f6b3beab
plegall <plg@piwigo.org>
2013-06-11 21:25:29 +0000

    new plugin to select photo size before download
    git-svn-id: http://piwigo.org/svn/extensions/download_by_size@23157 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ main.inc.php
@@ -0,0 +1,63 @@
+<?php
+/*
+Plugin Name: Download by Size
+Version: auto
+Description: Select a photo size before download
+Plugin URI: http://piwigo.org/ext/extension_view.php?eid=
+Author: plg
+Author URI: http://le-gall.net/pierrick
+*/
+
+if (!defined('PHPWG_ROOT_PATH'))
+{
+  die('Hacking attempt!');
+}
+
+define('DLSIZE_PATH' , PHPWG_PLUGINS_PATH.basename(dirname(__FILE__)).'/');
+
+add_event_handler('loc_end_picture', 'dlsize_picture');
+function dlsize_picture()
+{
+  global $conf, $template, $picture;
+  
+  $template->set_prefilter('picture', 'dlsize_picture_prefilter');
+
+  $params = array(
+    'id' => $picture['current']['id'],
+    'part' => 'e',
+    'download' => null,
+    );
+  $base_dl_url = add_url_params(get_root_url().PHPWG_PLUGINS_PATH.'download_by_size/action.php', $params);
+  
+  $template->assign(
+    array(
+      'DLSIZE_URL' => $base_dl_url.'&amp;size=',
+      )
+    );
+
+  if ($conf['picture_download_icon'])
+  {
+    // even if original can't be downloaded, we set U_DOWNLOAD so that
+    // visitor can download smaller sizes
+    $template->append('current', array('U_DOWNLOAD' => '#'), true);
+    
+    if (!empty($picture['current']['download_url']))
+    {
+      $template->assign('DLSIZE_ORIGINAL', $picture['current']['download_url']);
+    }
+  }
+
+  $template->set_filename('dlsize_picture', realpath(DLSIZE_PATH.'picture.tpl'));
+  $template->parse('dlsize_picture');
+}
+
+function dlsize_picture_prefilter($content, &$smarty)
+{
+  $pattern = '#\{if isset\(\$current\.U_DOWNLOAD\)\}\s*<a #';
+  $replacement = '{if isset($current.U_DOWNLOAD)}<a id="downloadSizeLink" ';
+  $content = preg_replace($pattern, $replacement, $content);
+
+  return $content;
+}
+
+?>
